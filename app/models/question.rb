83029d57abcc8990143c8b9cec2a0d6007c3c9c0
David Guerette <david.guerette@gmail.com>
2014-06-20 14:40:41 -0400

    Updated question model to delete all associated answers if a question is deleted.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   validates :title, length: { minimum: 10 }, presence: true
   validates :description, length: { minimum: 20}, presence: true
